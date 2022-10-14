2771a345f805590d02c24678d6d2491020ec9b4d
Shamsul Azhar Ahmad Shamsuddin <shamsulazhar@gmail.com>
2022-10-14 18:29:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 cd /Users/shamsulazhar/Documents/odesk/jp/emas_my_scrapy 
 export PATH=$PATH:/Users/shamsulazhar/opt/miniconda3/envs/scrapy/bin
-echo $PATH
 export PYTHONPATH=/Applications
-/Users/shamsulazhar/opt/miniconda3/envs/scrapy/bin/scrapy crawl emas_my
+/Users/shamsulazhar/opt/miniconda3/envs/scrapy/bin/scrapy crawl -L WARNING emas_my
